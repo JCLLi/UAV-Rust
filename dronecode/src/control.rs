30a69d4806693a1fac75f2b6e829d1f878bff2d8
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2023-02-16 14:42:36 +0100

    Print once per second, but read more often. This fixes the issue where the MPU produces garbage data: The MPU buffer was becoming full because it was not read often enough.

--- dronecode/src/control.rs
+++ dronecode/src/control.rs
@@ -9,43 +9,41 @@ use tudelft_quadrupel::time::{set_tick_frequency, wait_for_next_tick, Instant};
 use tudelft_quadrupel::uart::send_bytes;
 
 pub fn control_loop() -> ! {
-    set_tick_frequency(1);
+    set_tick_frequency(100);
     let mut last = Instant::now();
 
-    loop {
+    for i in 0.. {
         let _ = Blue.toggle();
         let now = Instant::now();
         let dt = now.duration_since(last);
         last = now;
 
-        send_bytes(format!("DTT: {:?}ms\n", dt.as_millis()).as_bytes());
-
         let motors = get_motors();
-        send_bytes(
-            format!(
-                "MTR: {} {} {} {}\n",
-                motors[0], motors[1], motors[2], motors[3]
-            )
-            .as_bytes(),
-        );
-
         let quaternion = read_dmp_bytes().unwrap();
         let ypr = YawPitchRoll::from(quaternion);
-        send_bytes(format!("YPR {} {} {}\n", ypr.yaw, ypr.pitch, ypr.roll).as_bytes());
-
         let (accel, _) = read_raw().unwrap();
-        send_bytes(format!("ACC {} {} {}\n", accel.x, accel.y, accel.z).as_bytes());
-
         let bat = read_battery();
-        send_bytes(format!("BAT {bat}\n").as_bytes());
-
         let pres = read_pressure();
-        send_bytes(format!("BAR {pres} \n").as_bytes());
 
-        send_bytes("\n".as_bytes());
+        if i % 100 == 0 {
+            send_bytes(format!("DTT: {:?}ms\n", dt.as_millis()).as_bytes());
+            send_bytes(
+                format!(
+                    "MTR: {} {} {} {}\n",
+                    motors[0], motors[1], motors[2], motors[3]
+                )
+                .as_bytes(),
+            );
+            send_bytes(format!("YPR {} {} {}\n", ypr.yaw, ypr.pitch, ypr.roll).as_bytes());
+            send_bytes(format!("ACC {} {} {}\n", accel.x, accel.y, accel.z).as_bytes());
+            send_bytes(format!("BAT {bat}\n").as_bytes());
+            send_bytes(format!("BAR {pres} \n").as_bytes());
+            send_bytes("\n".as_bytes());
+        }
 
         // wait until the timer interrupt goes off again
         // based on the frequency set above
         wait_for_next_tick();
     }
+    unreachable!();
 }
